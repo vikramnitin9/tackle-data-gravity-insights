bf0ca7d65ff16f8554ed739d215594538cfe16da
Vikram Nitin <vikramnitin9@gmail.com>
2022-08-03 14:58:29 -0400

    Modified RQ2

--- RQ2/daytrader_app/mono2micro-2/customize_db2_container.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo "(*) Copying DDL into the DB2 container ..."
-cp /tmp/tables/Table.ddl /database/config/db2inst1/
-
-echo "(*) Running DDL in the DB2 container ..."
-sudo -u db2inst1 bash -c "export PATH=\$PATH:\$HOME/sqllib/bin && export DB2INSTANCE=db2inst1 && db2 connect to tradedb && db2 -tvf \$HOME/Table.ddl"
-
-echo "(*) DB2 container setup complete ..."
